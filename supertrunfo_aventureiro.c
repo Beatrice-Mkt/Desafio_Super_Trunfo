f931a940467310579298ec4cd215c4e841eb2c27
Beatrice-Mkt <bealovesrm@gmail.com>
2025-08-05 14:27:49 -0300

    Add files via upload

---
+++ supertrunfo_aventureiro.c
@@ -0,0 +1,52 @@
+ #include <stdio.h>
+
+int main() {
+    // Carta 1 - Florianópolis
+    char estado1 = 'F';
+    char codigo1[] = "F01";
+    char nome1[] = "Florianopolis";
+    int populacao1 = 537211;
+    float area1 = 674.844;
+    float pib1 = 23550000000.0; // R$ 23,55 bilhões
+    int pontos_turisticos1 = 100;
+    float densidade1 = 796.05;
+    float pib_per_capita1 = 45602.98;
+
+    // Carta 2 - Porto Alegre
+    char estado2 = 'P';
+    char codigo2[] = "P02";
+    char nome2[] = "Porto Alegre";
+    int populacao2 = 1332570;
+    float area2 = 496.827;
+    float pib2 = 81560000000.0; // R$ 81,56 bilhões
+    int pontos_turisticos2 = 32;
+    float densidade2 = 2689.94;
+    float pib_per_capita2 = 49740.90;
+
+    // Exibição da Carta 1
+    printf("Carta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nome1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.3f km²\n", area1);
+    printf("PIB: R$ %.2f bilhões\n", pib1 / 1000000000.0);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+    printf("Densidade Populacional: %.2f habitantes por km²\n", densidade1);
+    printf("PIB per capita: R$ %.2f reais\n", pib_per_capita1);
+    printf("\n");
+
+    // Exibição da Carta 2
+    printf("Carta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nome2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.3f km²\n", area2);
+    printf("PIB: R$ %.2f bilhões\n", pib2 / 1000000000.0);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
+    printf("Densidade Populacional: %.2f habitantes por km²\n", densidade2);
+    printf("PIB per capita: R$ %.2f reais\n", pib_per_capita2);
+
+    return 0;
+}
